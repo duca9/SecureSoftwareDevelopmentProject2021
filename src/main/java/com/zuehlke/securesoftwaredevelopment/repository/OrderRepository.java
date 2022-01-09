47b3f1bb5f3336fcb56667a19c7b6edf4beedc57
Dusan Stijovic <dusanstijovic598@gmail.com>
2022-01-09 21:07:36 +0100

    Fixed SQL injection vulnerability when ordering food.

--- src/main/java/com/zuehlke/securesoftwaredevelopment/repository/OrderRepository.java
+++ src/main/java/com/zuehlke/securesoftwaredevelopment/repository/OrderRepository.java
@@ -1,13 +1,12 @@
 package com.zuehlke.securesoftwaredevelopment.repository;
 
 import com.zuehlke.securesoftwaredevelopment.domain.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +14,7 @@ import java.util.List;
 @Repository
 public class OrderRepository {
 
+    private static final Logger LOG = LoggerFactory.getLogger(OrderRepository.class);
     private DataSource dataSource;
 
     public OrderRepository(DataSource dataSource) {
@@ -45,42 +45,31 @@ public class OrderRepository {
         return new Food(id, name);
     }
 
+
     public void insertNewOrder(NewOrder newOrder, int userId) {
-        LocalDate date = LocalDate.now();
-        String sqlQuery = "INSERT INTO delivery (isDone, userId, restaurantId, addressId, date, comment)" +
-                "values (FALSE, " + userId + ", " + newOrder.getRestaurantId() + ", " + newOrder.getAddress() + "," +
-                "'" + date.getYear() + "-" + date.getMonthValue() + "-" + date.getDayOfMonth() + "', '" + newOrder.getComment() + "')";
-        try {
-            Connection connection = dataSource.getConnection();
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(sqlQuery);
+        if (newOrder != null && userId >= 0) {
+            LocalDate date = LocalDate.now();
+            String sqlQuery = "INSERT INTO delivery (isDone, userId, restaurantId, addressId, date, comment)" +
+                    "values (FALSE, ?, ?, ?, ?, ?)";
+            try (
+                    Connection connection = dataSource.getConnection();
+                    PreparedStatement statement = connection.prepareStatement(sqlQuery)
+            ) {
+                statement.setInt(1, userId);
+                statement.setInt(2, newOrder.getRestaurantId());
+                statement.setInt(3, newOrder.getAddress());
+                statement.setDate(4, Date.valueOf(date));
+                statement.setString(5, newOrder.getComment());
+                statement.executeUpdate();
 
-            sqlQuery = "SELECT MAX(id) FROM delivery";
-            ResultSet rs = statement.executeQuery(sqlQuery);
+                insertOrderFood(newOrder);
 
-            if (rs.next()) {
-
-                int deliveryId = rs.getInt(1);
-                sqlQuery = "INSERT INTO delivery_item (amount, foodId, deliveryId)" +
-                        "values";
-                for (int i = 0; i < newOrder.getItems().length; i++) {
-                    FoodItem item = newOrder.getItems()[i];
-                    String deliveryItem = "";
-                    if (i > 0) {
-                        deliveryItem = ",";
-                    }
-                    deliveryItem += "(" + item.getAmount() + ", " + item.getFoodId() + ", " + deliveryId + ")";
-                    sqlQuery += deliveryItem;
-                }
-                System.out.println(sqlQuery);
-                statement.executeUpdate(sqlQuery);
+            } catch (SQLException e) {
+                LOG.error("Error while inserting new order");
             }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } else {
+            LOG.error("Input params error");
         }
-
-
     }
 
     public Object getAddresses(int userId) {
@@ -104,5 +93,61 @@ public class OrderRepository {
         String name = rs.getString(2);
         return new Address(id, name);
 
+    }
+
+    private int getCurrentOrderId() {
+        String sqlQuery = "SELECT MAX(id) FROM delivery";
+        int retVal = -1;
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sqlQuery)
+        ) {
+            try (ResultSet rs = statement.executeQuery()) {
+                if (rs.next()) {
+                    retVal = rs.getInt(1);
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return retVal;
+    }
+
+    private void insertOrderFood(NewOrder newOrder) {
+        if (newOrder != null) {
+            int nextOrderId = getCurrentOrderId();
+            if (nextOrderId != -1) {
+                StringBuilder foodSqlQuery = new StringBuilder("INSERT INTO delivery_item (amount, foodId, deliveryId)" +
+                        "values");
+                List<Object> paramToSet = new ArrayList<>();
+                String separator = "";
+                for (int i = 0; i < newOrder.getItems().length; i++) {
+                    FoodItem item = newOrder.getItems()[i];
+                    String deliveryItem = separator + "(?, ?, ?)";
+                    foodSqlQuery.append(deliveryItem);
+                    separator = ",";
+                    paramToSet.add(item.getAmount());
+                    paramToSet.add(item.getFoodId());
+                    paramToSet.add(nextOrderId);
+                }
+                String query = foodSqlQuery.toString();
+                try (
+                        Connection connection = dataSource.getConnection();
+                        PreparedStatement statement = connection.prepareStatement(query)
+                ) {
+                    int paramIndex = 1;
+                    for (Object oneParam : paramToSet) {
+                        statement.setObject(paramIndex++, oneParam);
+                    }
+                    statement.executeUpdate();
+                } catch (SQLException exception) {
+                    LOG.error("Error while inserting food order");
+                }
+            } else {
+                LOG.error("Error while getting current order id");
+            }
+        } else {
+            LOG.error("Order is null");
+        }
     }
 }
